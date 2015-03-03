d46d836e5f698d04817f5d046a2b57dbb62b3f14
anask <katib87@gmail.com>
2015-03-03 15:17:35 -0600

    Create website layout

---
+++ visualize/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+def land(request):
+	return render_to_response('visualize.html', { 'TITLE': 'Visualize Query'})
